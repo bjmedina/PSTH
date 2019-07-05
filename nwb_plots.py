e3a1fa33b19c572a0924f5db7dedee4aeee6a107
Bryan Medina <bjmedina.cs@gmail.com>
2019-07-05 10:56:57 -0400

    EDIT: nwb_plots.py

--- nwb_plots.py
+++ nwb_plots.py
@@ -7,79 +7,165 @@ Created on Wed Jun 12 09:25:21 EDT 2019
 """
 ###### Imports ########
 from nwb_plots_functions import *
-from scipy.interpolate import CubicSpline
-from scipy.ndimage.filters import gaussian_filter1d
+from scipy.interpolate import LSQUnivariateSpline
 
 import h5py as h5
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pickle
+import sys
 ########################
 
-MOUSE_ID   = '424448'
-probe_name = 'probeA'
+###### UPDATE PATH #################################
+DIRECTORY = '/Users/bjm/Documents/CMU/Research/data'
+
+MOUSE_ID = '405751'
+
+# Get file from directory
+spikes_nwb_file = os.path.join(DIRECTORY, 'mouse' + MOUSE_ID + '.spikes.nwb')
+nwb = h5.File(spikes_nwb_file, 'r')
+
+probe_names = nwb['processing']
+
+# Length of stimulus and desired bin width
 start      = 0 # in ms
 end        = 2000 #in ms
 width      = 1 # ms
+msToSec    = 1000 # 1000 ms in 1 sec
+
+# Actual bins (for later use)
 bins       = np.linspace(start, end, int( (end - start)/width + 1 ))
-num_trials = 630 # You should just get this from the data...
+
+# Number of times timulus is presented
+num_trials = 630 
     
 # All possible orientations of stimulus (angles and temporal frequencies)
 orientations = [i*45 for i in range(8)]
 temp_freqs   = [1, 2, 4, 8, 15]
 
-# File to get data from.
-filename = MOUSE_ID + "_" + probe_name
+xs = np.linspace(0,2000,10000)
 
-# plot directory
-PLOTS = '/Users/bjm/Documents/CMU/Research/data/plots/'
+# Knots for spline (selected by eye)
+knots = [0, 30, 50, 70, 100, 150, 200, 250, 300, 325, 375, 400]
 
+# Allows plotting (takes more time)
+PLOTTING = False
 
-try:
-    with open(filename, 'rb') as f:
-        probe = pickle.load(f)
+# Print Descriptions
+DESCRIPTIONS = True
+
+# Store firing rates per neuron depending on whether it's "high", "medium", or "low"
+for probe_name in probe_names:
+    # File to get data from.
+    probe_filename = MOUSE_ID + "_" + probe_name
+    print(probe_filename)
+    
+    # plot directory
+    ## CHANGE ME
+    PROBE_PLOTS_DIRECTORY = '/Users/bjm/Documents/CMU/Research/data/plots/probes/'
+    CELL_PLOTS_DIRECTORY  = '/Users/bjm/Documents/CMU/Research/data/plots/cells/' + probe_name + '/'
+    
+    ## Loop this
+    try:
+        with open(probe_filename, 'rb') as f:
+            probe = pickle.load(f)
+            
+    except FileNotFoundError:
+        saveProbeData(MOUSE_ID, probe_name, nwb)
+        print("Run again")
+        sys.exit(1)
         
-except FileNotFoundError:
-    saveData(MOUSE_ID)
-    print("Run again")
 
-# Summary of all activity across all cells in a probe.
-x = np.zeros((len(bins), 1))
+    # Summary of all activity across all cells in a probe.
+    x = np.zeros((len(bins), 1))
+    
+    # Getting all data for a given cell
+    for cell in probe.getCellList():
+        # current cell spiking data
+        curr_cell = np.zeros((len(bins), 1))
+        for freq in temp_freqs:
+            for angle in orientations:
+                config = str(freq) + "_" + str(angle)
+                curr_cell += probe.getCell(cell).getSpikes(config)
+                # Plot curr cell
+                x += probe.getCell(cell).getSpikes(config)
 
-# Getting all data
-for cell in probe.getCellList():
-    for freq in temp_freqs:
-        for angle in orientations:
-            config = str(freq) + "_" + str(angle)
-            x = x + probe.getCell(cell).getSpikes(config)
+        
+        z = fromFreqList(curr_cell)
+        curr_cell,b,c = plt.hist(z, bins)
+        plt.clf()
+        curr_cell /= num_trials*0.001
+        probe.getCell(cell).max_frate = max(curr_cell[0:500])
+        probe.getCell(cell).avg_frate = np.mean(curr_cell[0:500])
+        probe.getCell(cell).std       = np.std(curr_cell[0:500])
 
-### Improve this
-z = []
-for i in range(len(x)):
-    y = [ i for ii in range(int(x[i])) ]
-    for num in y:
-        z.append(num)
+        # TODO
+        # ----
+        # Find valleys, peaks, and when they occur..
 
-x,b,c = plt.hist(z, bins)
-plt.clf()
-###
+        if(DESCRIPTIONS):
+            print("%s\t Max: %3.2f\t Avg: %3.2f\t Std: %3.2f" % (cell, max(curr_cell), np.mean(curr_cell), np.std(curr_cell))) 
+        
+        lsq = LSQUnivariateSpline(bins[0:len(bins)-1], curr_cell, knots[1:-1])
+        
+        # Plotting
+        if(PLOTTING):
+            # Plotting normalized cell activity
+            cell_filename  = MOUSE_ID + "_cell" + str(cell)
+            print(cell_filename)
+            plt.ylim(0, 75)
+            plt.xlim(-20, 520)
+            plt.ylabel('Spikes/second')
+            plt.xlabel('Bins')
+            plt.title("Mouse: " + str(MOUSE_ID) + " / " +  probe_name + " in "+ probe.name + ". Cell: " + str(cell))
+            plt.plot(xs, lsq(xs), color = 'magenta', alpha=0.76) 
+            plt.bar(b[0:len(b)-1], curr_cell)
+            plt.savefig(CELL_PLOTS_DIRECTORY + cell_filename + ".png")
+            plt.clf()
+    
 
-### Normalization
-# also divide by number of neurons in that particular region
-x /= num_trials*(0.0001)*len(probe.getCellList()) #Should I divide by number of cells?
+    # Plotting normalized probe activity
+    z = fromFreqList(x)
+    
+    x,b,c = plt.hist(z, bins)
+    plt.clf()
+    ###
+    
+    ### Normalization
+    # also divide by number of neurons in that particular region
+    x /= num_trials*(0.001)*len(probe.getCellList())
+    probe.max_frate = max(x[0:500])
+    probe.max_ftime = np.where(x == probe.max_frate)[0]
+    probe.avg_frate = np.mean(x[0:500])
+    probe.std       = np.std(x[0:500])
 
-# Smoothing (with cubic spline)
-cs = CubicSpline(bins[0:len(bins)-1], x)    
-plt.plot(bins, cs(bins)) 
+    if(DESCRIPTIONS):
+        print("%s\t Max: %3.2f @ %d\t Avg: %3.2f\t Std: %3.2f." % (probe.name, probe.max_frate, probe.max_ftime, probe.avg_frate, probe.std))
+        
+    
+    # Smoothing 
+    lsq = LSQUnivariateSpline(bins[0:len(bins)-1], x, knots[1:-1])
 
+    if(PLOTTING):
+        # Plotting
+        plt.ylim(0, 12)
+        plt.xlim(-20, 500)
+        plt.ylabel('Spikes/second')
+        plt.xlabel('Bins')
+        plt.title("Mouse: " + str(MOUSE_ID) + " / " +  probe_name + " in "+ probe.name)
+        plt.plot(xs, lsq(xs), color = 'red') 
+        plt.bar(b[0:len(b)-1], x, alpha=0.8)
+        plt.savefig(PROBE_PLOTS_DIRECTORY + probe_filename + ".png")
+        
+        plt.clf()
 
-# Plotting
-plt.title(filename + ": " + probe.name)
-plt.bar(b[0:len(b)-1], x)
-plt.savefig(PLOTS + filename + ".png")
-
-
-'''smoothing
------------
+    with open(probe_filename, 'wb') as f:
+        pickle.dump(probe, f)
+'''
+TODO: 
+MORT1dSMOOTH
+15.2.3 (leading upto as well)
+BARS (poisson version)
+419
 '''
