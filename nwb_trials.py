a1fe3caa2685d6eac1e77ee5aa59fe31bf00ba8d
Bryan Medina <bjmedina.cs@gmail.com>
2019-07-30 12:49:11 -0400

    Bootstrapping functional. TODO: Get confidence intervals (of points and of differences)

--- nwb_trials.py
+++ nwb_trials.py
@@ -19,7 +19,8 @@ This file
 DIRECTORY   = '/Users/bjm/Documents/CMU/Research/data/'
 TRIAL_DATA  = '/Users/bjm/Documents/CMU/Research/data/trial_data/'
 TRIAL_PLOTS = '/Users/bjm/Documents/CMU/Research/data/plots/trials/'
-MOUSE_ID    = '424448'
+TMAX_DIREC  = '/Users/bjm/Documents/CMU/Research/data/tmax/'
+MOUSE_ID    = '421338'
 ##########################################################################
 
 
@@ -29,8 +30,13 @@ nwb = h5.File(spikes_nwb_file, 'r')
 
 probe_names = nwb['processing']
 
-# I should probably make a trial data structure, then fill that data struct with with a trial number, the t set and the beta set, the configuration of that trial
+# Whether or not we want to calculate the confidence intervals
+CONF_INTERVAL = True
+BOOTSTRAPS = 500
 
+trials = []
+t_max1 = []
+t_max2 = []
 
 # Changes depending on the trial.
 start     = 0 #in second
@@ -41,7 +47,7 @@ end       = 2000 #in seconds
 timestamps  = nwb['stimulus']['presentation']['drifting_gratings_2']['timestamps'].value
 stim_orient = nwb['stimulus']['presentation']['drifting_gratings_2']['data'].value
 
-PLOTTING = True
+PLOTTING = False
 ## For every region,
 for probe_name in probe_names:
 
@@ -65,8 +71,9 @@ for probe_name in probe_names:
 
         try: 
             trial_file = TRIAL_DATA + "/" +  MOUSE_ID + "/" + probe_name + "/tr_" + str(trial_number)
-            with open(trial_file+")", 'rb') as t:
+            with open(trial_file, 'rb') as t:
                 tr = pickle.load(t)
+                trials.append(tr)
 
         except FileNotFoundError:
             trial = timestamps[trial_number]
@@ -116,7 +123,7 @@ for probe_name in probe_names:
                 tr.lsq    = LSQUnivariateSpline(bins[0:len(bins)-1], curr_trial, knots)
                 #tr.lsq    = UnivariateSpline(bins[0:len(bins)-1], curr_trial)
 
-                # Find the 
+                trials.append(tr)
                 #######################
                 with open(trial_file, 'wb') as t:
                     pickle.dump(tr, t)
@@ -133,3 +140,75 @@ for probe_name in probe_names:
             #plt.savefig(TRIAL_PLOTS + MOUSE_ID + "/" + probe_name + "/" + "tr_"+str(trial_number))
             plt.clf()
             
+
+
+    if(CONF_INTERVAL):
+        # Calculating the confidence intervals
+        fname = TMAX_DIREC + MOUSE_ID + "/" + probe_name + "/" + MOUSE_ID + "_tmax_"
+
+        try:
+            with open(fname + "1", 'rb') as f:
+                t_max1 = pickle.load(f)
+
+            with open(fname + "2", 'rb') as f:
+                t_max2 = pickle.load(f)
+
+        except FileNotFoundError:
+            # We're doing 500 bootstraps
+            for i in range(0, BOOTSTRAPS):
+                print("BOOTSTRAP %d" % i)
+                # g is going to be our random sample, size 600, of the 600 trials
+                g = choices(trials, k = len(trials))
+                
+                sample_spikes = np.zeros((len(g[0].spikes),))
+                lsq = np.zeros((len(g[0].lsq(xs)), 1))
+                
+                # Now we need to construct our curves based on these 600 samples
+                for sample in g:
+                    # Need to add all spikes together
+                    ## To do this, we have to *essentially* do an element wise addition
+                    for j in range(0, len(sample.spikes)):
+                        sample_spikes[j] += sample.spikes[j]
+
+                # Recompute tmax_1 and tmax_2
+                ## We have to normalize sample_spikes by number of trials
+                sample_spikes /= len(g)
+                peak  = max(sample_spikes[0:500])
+                tmax_1 = np.where(sample_spikes[0:500] == peak)[0][0]
+                
+                peak2 = max(sample_spikes[200:300])
+                tmax_2 = np.where(sample_spikes[200:300] == peak2)[0][0] + 200
+                
+                if(PLOTTING):
+                    print("Peak 1: %d @ %d" % (peak, tmax_1))
+                    print("Peak 2: %d @ %d" % (peak, tmax_2))
+                    plt.ylim(0, 10)
+                    plt.xlim(-2, 500)
+                    plt.bar(bins[:-1], sample_spikes, alpha=0.8, color='blue')
+                    plt.axvline(x=tmax_1,color='red', linestyle='--')
+                    plt.axvline(x=tmax_2,color='red', linestyle='--')
+                    
+                    plt.show()
+                    plt.clf()
+            
+                # Save those two into two separate vectors
+                t_max1.append(tmax_1)
+                t_max2.append(tmax_2)
+            
+
+    
+    # clear the slate for the next probe         
+    trials = []
+    
+    with open(fname + "1", 'wb') as f:
+        pickle.dump(t_max1, f)
+
+        
+    with open(fname + "2", 'wb') as f:
+        pickle.dump(t_max2, f)
+
+    t_max1 = []
+    t_max2 = []
+
+    
+fname = TMAX_DIREC + MOUSE_ID + "/" + probe_name + "/" + MOUSE_ID + "_tmax_"
